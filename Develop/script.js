a304c9ecd040481c242cdbdd33eeacfdc5e4c724
Izzie <ismael.espana88@gmail.com>
2021-10-12 09:54:47 -0700

    cleaned up generated password section

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,4 @@
 // Assignment code here
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 function generatePassword() {
   var includeUppercase = document.querySelector("#uppercase").checked;
   var includeLowercase = document.querySelector("#lowercase").checked;
@@ -24,18 +10,36 @@ function generatePassword() {
   var alphaCharacters = "abcdefghijklmnopqrstuvwxyz"; //to upper 
   var numericCharacters = '0123456789';
   var specialCharacters = ' !\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
-  var characterOptions = "";
-
-  characterOptions += includeUppercase ? alphaCharacters.toUpperCase() : "";
-  characterOptions += includeLowercase ? alphaCharacters : "";
-  characterOptions += includeNumeric ? numericCharacters : "";
-  characterOptions += includeSpecialCharacters ? specialCharacters : "";
 
   while (generatedPassword.length < passwordLength) {
-    generatedPassword += characterOptions.charAt(Math.floor(Math.random() * generatedPassword.length));
 
+    lowercaseCharacter = Math.ceil(alphaCharacters.length * Math.random() * Math.random());
+    uppercaseCharacter = Math.ceil(alphaCharacters.length * Math.random() * Math.random());
+    numericCharacter = Math.ceil(numericCharacters.length * Math.random() * Math.random());
+    specialCharacter = Math.ceil(specialCharacters.length * Math.random() * Math.random());
+    generatedPassword += includeUppercase && (generatedPassword.length < passwordLength) ? alphaCharacters.charAt(uppercaseCharacter).toUpperCase() : "";
+    generatedPassword += includeLowercase && (generatedPassword.length < passwordLength) ? alphaCharacters.charAt(lowercaseCharacter) : "";
+    generatedPassword += includeNumeric && (generatedPassword.length < passwordLength) ? numericCharacters.charAt(numericCharacter) : "";
+    generatedPassword += includeSpecialCharacters && (generatedPassword.length < passwordLength) ? specialCharacters.charAt(specialCharacter) : "";
+    
   }
+
+  generatedPassword = generatedPassword.split('').sort(function () { return 0.5 - Math.random() }).join('');
+
   return generatedPassword;
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 
 }
 
